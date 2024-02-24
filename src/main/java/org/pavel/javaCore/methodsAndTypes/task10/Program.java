c425d3dc07fabfa55f9336ebf0ef7e98bb895626
PavelDoldinn <asdq59asdq@yandex.ru>
2024-02-24 17:54:16 +0500

    /home/pavel/IdeaProjects/skillSpace/src/main/java/org/pavel/javaCore/methodsAndTypes/task10/Program.java+

--- src/main/java/org/pavel/javaCore/methodsAndTypes/task10/Program.java
+++ src/main/java/org/pavel/javaCore/methodsAndTypes/task10/Program.java
@@ -1,27 +1,26 @@
 package org.pavel.javaCore.methodsAndTypes.task10;
 
+//Задать целое число в виде переменной, это число – сумма денег в рублях.
+//Вывести это число на экран, добавив к нему слово «рублей» в правильном падеже.
 public class Program {
 
     public static void main(String[] args) {
 
-        //Задать целое число в виде переменной, это число – сумма денег в рублях.
-        //Вывести это число на экран, добавив к нему слово «рублей» в правильном падеже.
-
-        amountOfRubles(0);
+        amountOfRubles(111);
     }
 
     static void amountOfRubles(int rubles) {
 
-        if (rubles == 1) {
+        int res = rubles % 100 / 10;
+        if (res == 1) {
+            System.out.println(rubles + " Рублей.");
 
+        } else if (rubles % 10 == 1) {
             System.out.println(rubles + " Рубль.");
-
-        } else if (rubles == 2 || rubles == 3 || rubles == 4) {
+        } else if (rubles % 10 == 2 || rubles % 10 == 3 || rubles % 10 == 4) {
             System.out.println(rubles + " Рубля.");
         } else {
             System.out.println(rubles + " Рублей.");
         }
-
     }
-
 }
