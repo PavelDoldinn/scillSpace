f1e01692dd505a722b67bf1b4c9ecd78f6fdbd6a
PavelDoldinn <asdq59asdq@yandex.ru>
2024-03-06 13:57:40 +0500

    add_/home/pavel/IdeaProjects/skillSpace/src/main/java/org/pavel/algorithms/primeNumbers/GetPrimeNumbers.java

---
+++ src/main/java/org/pavel/algorithms/primeNumbers/GetPrimeNumbers.java
@@ -0,0 +1,23 @@
+package org.pavel.algorithms.primeNumbers;
+
+/**
+ * Выведет все простые числа от 0 до n
+ */
+public class GetPrimeNumbers {
+
+    public void getPrimeNumbers(int number){
+
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i / 2 + 1; j++) {
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
---
+++ src/main/java/org/pavel/algorithms/primeNumbers/Program.java
@@ -0,0 +1,11 @@
+package org.pavel.algorithms.primeNumbers;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        GetPrimeNumbers getPrimeNumbers = new GetPrimeNumbers();
+        getPrimeNumbers.getPrimeNumbers(1000);
+    }
+
+}
